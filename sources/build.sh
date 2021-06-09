237e1e9eb3b86ecdc5e54ab6b52b99a5f8610139
Rosalie Wagner <mail@rosaliewagner.com>
2021-06-09 13:25:42 +0200

    updated build system

--- sources/build.sh
+++ sources/build.sh
@@ -1,3 +1,18 @@
+#!/bin/sh
+set -e
+
+echo ".
+GENERATING FONTS
+."
 gftools builder config.yaml
-cd ../scripts/
-python3 postprocess.py
+
+
+echo ".
+SLICING ROMAN/ITALIC
+."
+fonttools varLib.instancer ../fonts/variable/Urbanist[ital,wght].ttf ital=0 -o ../fonts/variable/Urbanist[wght].ttf --update-name-table
+fonttools varLib.instancer ../fonts/variable/Urbanist[ital,wght].ttf ital=1 -o ../fonts/variable/Urbanist-Italic[wght].ttf --update-name-table
+
+echo ".
+COMPLETE
+."
