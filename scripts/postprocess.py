bdbc943a411728ef83a0c94d5aef524ad0aed2a0
corey hu <corey.huym@gmail.com>
2021-07-10 17:22:02 -0700

    Fix postscript prefix in name table for Roman/Italic instances

--- scripts/postprocess.py
+++ scripts/postprocess.py
@@ -1,11 +1,12 @@
 import re
 import os
 import glob
+import shutil
 from fontTools import ttLib
 from fontTools.varLib import instancer
 from fontTools.ttLib import TTFont
 from fontTools.ttLib.ttFont import newTable
-from fontbakery.constants import MacStyle, FsSelection, NameID, PlatformID, WindowsEncodingID, WindowsLanguageID
+from fontbakery.constants import MacStyle, FsSelection, NameID, PlatformID, WindowsEncodingID, WindowsLanguageID, MacintoshEncodingID, MacintoshLanguageID
 
 def fix_italic_angle(ttFont, angle=-8.0):
   post = ttLib.getTableClass("post")()
@@ -30,31 +31,86 @@ def add_avar_table(ttFont):
   }
   ttFont['avar'] = avar
 
-def set_windows_subfamily_name(ttFont, subfamily_name):
+def fix_postscript_names(ttFont, ps_nameids, prefix):
+
   ttFont['name'].setName(
-    subfamily_name, 
-    NameID.FONT_SUBFAMILY_NAME,
+    prefix, 
+    25,
     PlatformID.WINDOWS,
     WindowsEncodingID.UNICODE_BMP,
     WindowsLanguageID.ENGLISH_USA
   )
 
+  ttFont['name'].setName(
+    prefix, 
+    25,
+    PlatformID.MACINTOSH,
+    MacintoshEncodingID.ROMAN,
+    MacintoshLanguageID.ENGLISH
+  )
+
+  for nameid in ps_nameids:
+    cur_name = ttFont['name'].getName( 
+      nameid,
+      PlatformID.WINDOWS,
+      WindowsEncodingID.UNICODE_BMP,
+      WindowsLanguageID.ENGLISH_USA
+    )
+    suffix = str(cur_name).split("-")[-1]
+    if suffix.endswith("Italic"):
+      suffix = suffix[:-6]
+    
+    if suffix == "":
+      suffix = "Regular"
+
+    new_name = "{}-{}".format(prefix, suffix)
+    print("Renaming instance {} to {}".format(str(cur_name), new_name))
+
+    ttFont['name'].setName(
+      new_name, 
+      nameid,
+      PlatformID.WINDOWS,
+      WindowsEncodingID.UNICODE_BMP,
+      WindowsLanguageID.ENGLISH_USA
+    )
+
+    ttFont['name'].setName(
+      new_name, 
+      nameid,
+      PlatformID.MACINTOSH,
+      MacintoshEncodingID.ROMAN,
+      MacintoshLanguageID.ENGLISH
+    )    
+
 
 if __name__ == "__main__":
-  # for font_path in glob.glob("../fonts/variable/Urbanist-*.ttf"):
-  #   # Rename Urbanist font in variable directory
-  #   new_path = re.sub(r"Urbanist-.*?\[", "Urbanist[", font_path)
-  #   os.rename(font_path, new_path)
-  #   print("Renaming {} to {}".format(font_path, new_path))
 
-  # for font_path in glob.glob("../fonts/*variable/*.ttf"):
-  #   # Add avar table to all variable fonts
-  #   with open(font_path, "rb") as f:
-  #     print("Add avar table to {}".format(font_path))
-  #     ttFont = TTFont(f)
-  #     add_avar_table(ttFont)
-  #     ttFont.save(font_path)
+  roman_path = "../fonts/variable/Urbanist[wght].ttf"
+  with open(roman_path, "rb") as f:
+    # Fix roman postscript names
+    ttFont = TTFont(f)
+    ps_nameids = [instance.postscriptNameID for instance in ttFont['fvar'].instances]
+    fix_postscript_names(ttFont, ps_nameids, "UrbanistRoman")
+    
+    # Fix OS/2.fsSelection bit 7
+    
+    
+    ttFont.save(roman_path)
+
+
   
+  italic_path = "../fonts/variable/Urbanist-Italic[wght].ttf"
+  with open(italic_path, "rb") as f:
+    # Fix italic postscript names
+    ttFont = TTFont(f)
+    ps_nameids = [instance.postscriptNameID for instance in ttFont['fvar'].instances]
+    fix_postscript_names(ttFont, ps_nameids, "UrbanistItalic")
+
+    # Fix OS/2.fsSelection bit 7
+
+    ttFont.save(italic_path)
+
+
   for font_path in glob.glob("../fonts/variable/Urbanist-Italic*.ttf"):
     # set windows subfamily name to Italic in name table
     # For Urbanist-Italic, set ttFont["head"].macStyle = MacStyle.ITALIC
@@ -62,15 +118,6 @@ if __name__ == "__main__":
     with open(font_path, "rb") as f:
       print("Fix italic naming in {}".format(font_path))
       ttFont = TTFont(f)
-      set_windows_subfamily_name(ttFont, "Italic")
       ttFont["head"].macStyle = MacStyle.ITALIC
-      ttFont['OS/2'].fsSelection = FsSelection.ITALIC
+      ttFont['OS/2'].fsSelection = FsSelection.USETYPOMETRICS | FsSelection.ITALIC
       ttFont.save(font_path)
-
-  # for font_path in glob.glob("../fonts/ttf/*Italic.ttf"):
-  #   # Fix Italic Angle
-  #   with open(font_path, "rb") as f:
-  #     print("Fix italic angle in {}".format(font_path))
-  #     ttFont = TTFont(f)
-  #     fix_italic_angle(ttFont)
-  #     ttFont.save(font_path)
--- sources/build.sh
+++ sources/build.sh
@@ -10,15 +10,14 @@ gftools builder config.yaml
 echo ".
 SLICING ROMAN/ITALIC
 ."
-fonttools varLib.instancer ../fonts/variable/Urbanist[ital,wght].ttf ital=0 -o ../fonts/variable/Urbanist[wght].ttf --update-name-table
-fonttools varLib.instancer ../fonts/variable/Urbanist[ital,wght].ttf ital=1 -o ../fonts/variable/Urbanist-Italic[wght].ttf --update-name-table
 
+fonttools varLib.instancer ../fonts/variable/Urbanist\[ital,wght\].ttf ital=0 -o ../fonts/variable/Urbanist\[wght\].ttf --update-name-table
+fonttools varLib.instancer ../fonts/variable/Urbanist\[ital,wght\].ttf ital=1 -o ../fonts/variable/Urbanist-Italic\[wght\].ttf --update-name-table
 
 echo ".
 POST-PROCESSING ADJUSTMENTS
 ."
-cd ../scripts 
-python3 postprocess.py 
+python3 ../scripts/postprocess.py 
 
 echo ".
 COMPLETE
