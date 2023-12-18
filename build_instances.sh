6996d75fa887c93075c82f4edfdf2788487765ec
corey hu <corey@coreyhu.com>
2023-12-17 22:54:44 -0800

    build instance fonts

---
+++ build_instances.sh
@@ -0,0 +1,2 @@
+fonttools varLib.instancer fonts/variable/Urbanist\[ital,wght\].ttf ital=0 -o fonts/variable/Urbanist\[wght\].ttf --update-name-table
+fonttools varLib.instancer fonts/variable/Urbanist\[ital,wght\].ttf ital=1 -o fonts/variable/Urbanist-Italic\[wght\].ttf --update-name-table
