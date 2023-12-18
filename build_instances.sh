cc41f618a75675a45fd9e863e2d50fec3146793f
corey hu <corey@coreyhu.com>
2023-12-18 09:29:36 -0800

    add shebang

--- build_instances.sh
+++ build_instances.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 fonttools varLib.instancer fonts/variable/Urbanist\[ital,wght\].ttf ital=0 -o fonts/variable/Urbanist\[wght\].ttf --update-name-table
 fonttools varLib.instancer fonts/variable/Urbanist\[ital,wght\].ttf ital=1 -o fonts/variable/Urbanist-Italic\[wght\].ttf --update-name-table
